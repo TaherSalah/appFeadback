9b75803e07c91321ee294c90f77bee5d267359d4
TaherSalah <tahersalah2016@gmail.com>
2025-12-16 02:01:37 +0200

    add inhance some edits and new features

--- lib/app/features/QiblaView/ARQiblaCameraWidget.dart
+++ lib/app/features/QiblaView/ARQiblaCameraWidget.dart
@@ -1,5 +1,6 @@
 
 import 'dart:math' as math;
+import 'dart:ui';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
